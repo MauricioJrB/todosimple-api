1128d6ae7f287daf35787ec0775b563d993103ac
Mauricio Junior <mjunior1205@gmail.com>
2024-02-27 18:46:45 -0300

    Create Task model

--- src/main/java/com/demo/todosimples/models/User.java
+++ src/main/java/com/demo/todosimples/models/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -20,43 +21,40 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
-
     public interface CreateUser {}
 
     public interface UpdateUser {}
 
     public static final String TABLE_NAME = "user";
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", nullable = false, unique = true)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
-    
+
     @JsonProperty(access = Access.WRITE_ONLY)
-    @Column(name = "password", nullable = false)
+    @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = { CreateUser.class, UpdateUser.class })
     @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
-    @Size(groups = { CreateUser.class, UpdateUser.class },min = 8, max = 60)
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<>();
-
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {}
-
 
     public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
     }
-
 
     public Long getId() {
         return this.id;
@@ -80,6 +78,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
